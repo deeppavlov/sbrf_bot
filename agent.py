75a029f802a0442578c73a6ee5c324031adac817
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2018-07-30 15:58:16 +0300

    feat: add 'hello' skill

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 from deeppavlov.core.commands.infer import build_model_from_config
 from deeppavlov.core.agent import Agent, HighestConfidenceSelector
+from deeppavlov.skills.pattern_matching_skill import PatternMatchingSkill
 
 from filter import IntentFilter
 
@@ -13,11 +14,16 @@ tarifs = build_model_from_config(json.load(open('skill.tarifs.json')), as_compon
 
 classifier = build_model_from_config(json.load(open('intent_classifier.json')), as_component=True)
 
-intents = ['DEMO', 'SMS_INFORM', 'TARIFS']
+hello = PatternMatchingSkill(
+    responses=['Привет! Я SBRF Demo Бот :) Чем вам помочь? Я могу рассказть про открытие и резервирование счетов, тарифы на услуги и продуты, а так же подключить SMS-информирование. '],
+    patterns=[".*"]
+)
+
+intents = ['HELLO', 'DEMO', 'SMS_INFORM', 'TARIFS']
 
 filter = IntentFilter(intents, classifier, default_intent=0)
 
-agent = Agent([demo, sms_inform, tarifs], skills_selector=HighestConfidenceSelector(), skills_filter=filter)
+agent = Agent([hello, demo, sms_inform, tarifs], skills_selector=HighestConfidenceSelector(), skills_filter=filter)
 
 for line in sys.stdin:
     print(agent([line]), flush=True)
